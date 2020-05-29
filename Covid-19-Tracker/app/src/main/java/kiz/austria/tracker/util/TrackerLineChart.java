56951c5ea7a001bf76334d254c98bd64ac8215f8
Vel01 <arielaustria01@gmail.com>
2020-05-29 08:30:15 +0800

    philippines implementation (PHTrend using asyncTask)

--- Covid-19-Tracker/app/src/main/java/kiz/austria/tracker/util/TrackerLineChart.java
+++ Covid-19-Tracker/app/src/main/java/kiz/austria/tracker/util/TrackerLineChart.java
@@ -1,5 +1,7 @@
 package kiz.austria.tracker.util;
 
+import android.os.AsyncTask;
+
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
@@ -11,14 +13,13 @@ import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import kiz.austria.tracker.model.PHTrend;
 
 import static android.graphics.Color.rgb;
 
-public class TrackerLineChart {
+public class TrackerLineChart implements DataInfected.OnDataInfectedAvailable {
     private static final String TAG = "TrackerLineChart";
     private final LineChart chart;
     private final int[] colors = new int[]{
@@ -78,9 +79,9 @@ public class TrackerLineChart {
     private LineData data = new LineData();
 
     private ILineDataSet dataForInfected(List<PHTrend> trends) throws ParseException {
-        Collections.sort(trends, (o1, o2) ->
-                TrackerUtility.sort(Integer.parseInt(o2.getInfected()),
-                        Integer.parseInt(o1.getInfected())));
+//        Collections.sort(trends, (o1, o2) ->
+//                TrackerUtility.sort(Integer.parseInt(o2.getInfected()),
+//                        Integer.parseInt(o1.getInfected())));
         List<Entry> values = new ArrayList<>();
 
         float currentValue = -1;
@@ -108,9 +109,9 @@ public class TrackerLineChart {
     }
 
     private ILineDataSet dataForRecovered(List<PHTrend> trends) throws ParseException {
-        Collections.sort(trends, (o1, o2) ->
-                TrackerUtility.sort(Integer.parseInt(o2.getRecovered()),
-                        Integer.parseInt(o1.getRecovered())));
+//        Collections.sort(trends, (o1, o2) ->
+//                TrackerUtility.sort(Integer.parseInt(o2.getRecovered()),
+//                        Integer.parseInt(o1.getRecovered())));
         List<Entry> values = new ArrayList<>();
 
         float currentValue = -1;
@@ -138,9 +139,9 @@ public class TrackerLineChart {
     }
 
     private LineDataSet dataForDeceased(List<PHTrend> trends) throws ParseException {
-        Collections.sort(trends, (o1, o2) ->
-                TrackerUtility.sort(Integer.parseInt(o2.getDeceased()),
-                        Integer.parseInt(o1.getDeceased())));
+//        Collections.sort(trends, (o1, o2) ->
+//                TrackerUtility.sort(Integer.parseInt(o2.getDeceased()),
+//                        Integer.parseInt(o1.getDeceased())));
         List<Entry> values = new ArrayList<>();
 
         float currentValue = -1;
@@ -169,19 +170,155 @@ public class TrackerLineChart {
 
     public void setLineChartData(List<PHTrend> trends) throws ParseException {
 
-        TrackerSort.quickSort(trends, "INFECTED", 0, trends.size());
-        data.addDataSet(dataForInfected(trends));
+        DataInfected dataInfected = new DataInfected(this, trends, "INFECTED");
+        dataInfected.execute();
 
-        TrackerSort.quickSort(trends, "RECOVERED", 0, trends.size());
-        data.addDataSet(dataForRecovered(trends));
+        dataInfected = new DataInfected(this, trends, "RECOVERED");
+        dataInfected.execute();
 
-        TrackerSort.quickSort(trends, "DECEASED", 0, trends.size());
-        data.addDataSet(dataForDeceased(trends));
-        chart.setData(data);
-        chart.invalidate();
+        dataInfected = new DataInfected(this, trends, "DECEASED");
+        dataInfected.execute();
     }
 
     public LineChart getChart() {
         return chart;
+    }
+
+    @Override
+    public void onDataInfectedAvailable(ILineDataSet lineDataSet) {
+        data.addDataSet(lineDataSet);
+        chart.setData(data);
+        chart.invalidate();
+    }
+}
+
+class DataInfected extends AsyncTask<Void, Void, ILineDataSet> {
+    private final int[] colors = new int[]{
+            rgb(255, 68, 51),
+            rgb(151, 242, 149),
+            rgb(117, 117, 117)
+    };
+
+    private OnDataInfectedAvailable mDataInfectedAvailable;
+    private List<PHTrend> mPHTrends;
+    private String flag;
+
+    DataInfected(OnDataInfectedAvailable dataInfectedAvailable, List<PHTrend> PHTrends, String flag) {
+        mDataInfectedAvailable = dataInfectedAvailable;
+        mPHTrends = PHTrends;
+        this.flag = flag;
+    }
+
+    @Override
+    protected ILineDataSet doInBackground(Void... voids) {
+        try {
+            switch (flag) {
+                case "INFECTED":
+                    return dataForInfected(mPHTrends);
+                case "RECOVERED":
+                    return dataForRecovered(mPHTrends);
+                case "DECEASED":
+                    return dataForDeceased(mPHTrends);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(ILineDataSet lineDataSet) {
+        if (mDataInfectedAvailable != null) {
+            mDataInfectedAvailable.onDataInfectedAvailable(lineDataSet);
+        }
+    }
+
+    private ILineDataSet dataForInfected(List<PHTrend> trends) throws ParseException {
+        TrackerSort.quickSort(trends, flag, 0, trends.size());
+        List<Entry> values = new ArrayList<>();
+        float currentValue = -1;
+        String currentDate = "";
+        for (int j = 0; j < trends.size(); j++) {
+            PHTrend trend = trends.get(j);
+            float value = Float.parseFloat(trend.getInfected());
+            String date = TrackerUtility.formatSimpleDate(trend.getLatestUpdate());
+            if (currentValue != value && !currentDate.equals(date)) {
+                values.add(new Entry(j, value));
+                currentValue = value;
+                currentDate = date;
+            }
+        }
+
+        LineDataSet lineData = new LineDataSet(values, "Confirmed");
+        lineData.setLineWidth(1f);
+        lineData.setCircleRadius(2f);
+        lineData.setDrawCircleHole(false);
+
+        int color = rgb(255, 68, 51);
+        lineData.setColor(color);
+        lineData.setCircleColor(color);
+        return lineData;
+    }
+
+    private ILineDataSet dataForRecovered(List<PHTrend> trends) throws ParseException {
+        TrackerSort.quickSort(trends, flag, 0, trends.size());
+
+        List<Entry> values = new ArrayList<>();
+
+        float currentValue = -1;
+        String currentDate = "";
+        for (int j = 0; j < trends.size(); j++) {
+            PHTrend trend = trends.get(j);
+            float value = Float.parseFloat(trend.getRecovered());
+            String date = TrackerUtility.formatSimpleDate(trend.getLatestUpdate());
+            if (currentValue != value && !currentDate.equals(date)) {
+                values.add(new Entry(j, value));
+                currentValue = value;
+                currentDate = date;
+            }
+        }
+
+        LineDataSet lineData = new LineDataSet(values, "Recovered");
+        lineData.setLineWidth(1f);
+        lineData.setCircleRadius(2f);
+        lineData.setDrawCircleHole(false);
+
+        int color = colors[1];
+        lineData.setColor(color);
+        lineData.setCircleColor(color);
+        return lineData;
+    }
+
+    private LineDataSet dataForDeceased(List<PHTrend> trends) throws ParseException {
+        TrackerSort.quickSort(trends, flag, 0, trends.size());
+
+        List<Entry> values = new ArrayList<>();
+
+        float currentValue = -1;
+        String currentDate = "";
+        for (int j = 0; j < trends.size(); j++) {
+            PHTrend trend = trends.get(j);
+            float value = Float.parseFloat(trend.getDeceased());
+            String date = TrackerUtility.formatSimpleDate(trend.getLatestUpdate());
+            if (currentValue != value && !currentDate.equals(date)) {
+                values.add(new Entry(j, value));
+                currentValue = value;
+                currentDate = date;
+            }
+        }
+
+        LineDataSet lineData = new LineDataSet(values, "Deceased");
+        lineData.setLineWidth(1f);
+        lineData.setCircleRadius(2f);
+        lineData.setDrawCircleHole(false);
+
+        int color = colors[2];
+        lineData.setColor(color);
+        lineData.setCircleColor(color);
+        return lineData;
+    }
+
+    interface OnDataInfectedAvailable {
+        void onDataInfectedAvailable(ILineDataSet lineDataSet);
     }
 }
--- Covid-19-Tracker/app/src/main/java/kiz/austria/tracker/util/TrackerSort.java
+++ Covid-19-Tracker/app/src/main/java/kiz/austria/tracker/util/TrackerSort.java
@@ -1,79 +1,23 @@
 package kiz.austria.tracker.util;
 
-import java.util.Collections;
 import java.util.List;
 
 import kiz.austria.tracker.model.PHTrend;
 
 public class TrackerSort {
 
-//    public static void quickSort(List<PHTrend> trends, int start, int end) {
-//        if (end - start < 2) {
-//            return;
-//        }
-//
-//        int pivotIndex = partition(trends, start, end);
-//        quickSort(trends, start, pivotIndex);
-//        quickSort(trends, pivotIndex + 1, end);
-//    }
-//
-//    private static int partition(List<PHTrend> trends, int start, int end) {
-//        // This is using the first element as the pivot
-//        int pivot = Integer.parseInt(trends.get(start).getInfected());//input[start];
-//        int i = start;
-//        int j = end;
-//
-//        while (i < j) {
-//
-//            // NOTE: empty loop body
-//            while (i < j && Integer.parseInt(trends.get(--j).getInfected()) >= pivot) ;
-//            if (i < j) {
-//                //input[i] = input[j];
-//                Collections.swap(trends, i, j);
-//            }
-//
-//            // NOTE: empty loop body
-//            while (i < j && Integer.parseInt(trends.get(++i).getInfected()) <= pivot) ;
-//            if (i < j) {
-////                input[j] = input[i];
-//                Collections.swap(trends, j, i);
-//            }
-//
-//        }
-//
-////        input[j] = pivot;
-//        Collections.swap(trends, j, start);
-//        return j;
-//
-//    }
-
-
     public static void quickSort(List<PHTrend> trends, String flag, int start, int end) {
         if (end - start < 2) {
             return;
         }
-
         int pivotIndex = partition(trends, flag, start, end);
         quickSort(trends, flag, start, pivotIndex);
         quickSort(trends, flag, pivotIndex + 1, end);
     }
 
-    private static int setPivot(List<PHTrend> trends, String flag, int start) {
-        switch (flag) {
-            case "INFECTED":
-                return Integer.parseInt(trends.get(start).getInfected());//input[start];
-            case "RECOVERED":
-                return Integer.parseInt(trends.get(start).getRecovered());//input[start];
-            case "DECEASED":
-                return Integer.parseInt(trends.get(start).getDeceased());//input[start];
-        }
-        return 0;
-    }
-
-
     private static int partition(List<PHTrend> trends, String flag, int start, int end) {
         // This is using the first element as the pivot
-        int pivot = setPivot(trends, flag, start);
+        int pivot = trends.indexOf(trends.get(start));//input[start];
 
         int i = start;
         int j = end;
@@ -81,47 +25,62 @@ public class TrackerSort {
         while (i < j) {
 
             // NOTE: empty loop body
-//            while (i < j && Integer.parseInt(trends.get(--j).getInfected()) >= pivot) ;
             switch (flag) {
                 case "INFECTED":
-                    while (i < j && Integer.parseInt(trends.get(--j).getInfected()) >= pivot) ;
+                    while (i < j && Integer.parseInt(trends.get(--j).getInfected()) >= Integer.parseInt(trends.get(pivot).getInfected()))
+                        ;
                     break;
                 case "RECOVERED":
-                    while (i < j && Integer.parseInt(trends.get(--j).getRecovered()) >= pivot) ;
+                    while (i < j && Integer.parseInt(trends.get(--j).getRecovered()) >= Integer.parseInt(trends.get(pivot).getRecovered()))
+                        ;
                     break;
                 case "DECEASED":
-                    while (i < j && Integer.parseInt(trends.get(--j).getDeceased()) >= pivot) ;
+                    while (i < j && Integer.parseInt(trends.get(--j).getDeceased()) >= Integer.parseInt(trends.get(pivot).getDeceased()))
+                        ;
                     break;
             }
-
             if (i < j) {
-                //input[i] = input[j];
-                Collections.swap(trends, i, j);
+                trends.set(i, trends.get(j));
             }
 
             // NOTE: empty loop body
-//            while (i < j && Integer.parseInt(trends.get(++i).getInfected()) <= pivot) ;
             switch (flag) {
                 case "INFECTED":
-                    while (i < j && Integer.parseInt(trends.get(++i).getInfected()) >= pivot) ;
+                    while (i < j && Integer.parseInt(trends.get(++i).getInfected()) <= Integer.parseInt(trends.get(pivot).getInfected()))
+                        ;
                     break;
                 case "RECOVERED":
-                    while (i < j && Integer.parseInt(trends.get(++i).getRecovered()) >= pivot) ;
+                    while (i < j && Integer.parseInt(trends.get(++i).getRecovered()) <= Integer.parseInt(trends.get(pivot).getRecovered()))
+                        ;
                     break;
                 case "DECEASED":
-                    while (i < j && Integer.parseInt(trends.get(++i).getDeceased()) >= pivot) ;
+                    while (i < j && Integer.parseInt(trends.get(++i).getDeceased()) <= Integer.parseInt(trends.get(pivot).getDeceased()))
+                        ;
                     break;
             }
             if (i < j) {
-//                input[j] = input[i];
-                Collections.swap(trends, j, i);
+                trends.set(j, trends.get(i));
             }
 
         }
 
-//        input[j] = pivot;
-        Collections.swap(trends, j, start);
+        trends.set(j, trends.get(pivot));
         return j;
 
+    }
+
+
+    public static void insertionSort(List<PHTrend> trends) {
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < trends.size();
+             firstUnsortedIndex++) {
+            int newElement = trends.indexOf(trends.get(firstUnsortedIndex));
+
+            int i;
+
+            for (i = firstUnsortedIndex; i > 0 && Integer.parseInt(trends.get(i - 1).getInfected()) > Integer.parseInt(trends.get(newElement).getInfected()); i--) {
+                trends.set(i, trends.get(i - 1));
+            }
+            trends.set(i, trends.get(newElement));
+        }
     }
 }
